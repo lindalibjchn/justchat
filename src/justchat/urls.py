c6a17e25462d24ad4a2ab5643c775fb18171d12e
lindalibjchn <na.li2@ucdconnect.ie>
2020-07-17 12:10:00 +0100

    test

---
+++ src/justchat/urls.py
@@ -0,0 +1,16 @@
+# from django.contrib import admin
+# from django.urls import path, include
+#
+# urlpatterns = [
+#     path('chat', include('chat.urls', namespace='chat')),
+#     path('admin/', admin.site.urls)
+# ]
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('chat/', include('chat.urls', namespace='chat')),
+]
